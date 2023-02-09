210f47678b3ec8acaf19992069977644a9ad14fa
devcharles15531 <kingcharles15531@gmail.com>
2023-02-09 23:23:43 +0100

    Fixed event render state

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -60,10 +60,14 @@
                   >
                 </span>
               </li>
-              <li
+              <!-- <li
                 v-if="allTodaysEvent(day, events).length > 3"
                 @click="openModal(day, allTodaysEvent(day, events))"
-                class="text-gray-500"
+                class="text-gray-500 cursor-pointer"
+              > -->
+              <li
+                v-if="allTodaysEvent(day, events).length > 3"
+                class="text-gray-500 cursor-pointer"
               >
                 + {{ allTodaysEvent(day, events).length - 3 }} more
               </li>
@@ -131,147 +135,6 @@
     </div>
   </div>
 
-  <!-- <div
-        v-for="day in daysOfTheWeek"
-        class="text-center text-sm md:text-base lg:text-lg font-medium border"
-      >
-        {{ day.substring(0, 3) }}
-      </div>
-
-      <div
-        v-if="firstDayOfCurrentMonth > 0"
-        v-for="day in firstDayOfCurrentMonth"
-        :key="day"
-        class="h-16 md:h-36 w-full border opacity-50"
-      ></div>
-
-      <div
-        v-for="day in daysInCurrentMonth"
-        :key="day"
-        class="h-16 md:h-36 w-full border align-top"
-      >
-        <div
-          class="w-full h-full text-xs md:text-sm lg:text-base text-center transition-colors"
-          :class="{
-            'bg-slate-200 text-gray-600 font-medium': isToday(day),
-            'hover:bg-gray-100 hover:text-gray-700': !isToday(day),
-          }"
-        >
-          {{ day }}
-
-          <div
-            v-if="maxThreeTodaysEvent(day, events).length"
-            v-for="evt in maxThreeTodaysEvent(day, events)"
-            class="hidden md:block"
-          >
-            <div
-              class="w-full px-2 py-1 flex space-x-1 items-center whitespace-nowrap overflow-hidden hover:border hover:border-gray-200 cursor-pointer rounded-sm"
-              @click="togglePopover($event, evt)"
-            >
-              <div class="w-1/12">
-                <div class="h-2 w-2 rounded-full bg-purple-600"></div>
-              </div>
-              <div class="w-11/12">
-                <h5 class="text-xs tracking-tight text-clip overflow-hidden">
-                  {{ evt.title }}
-                </h5>
-              </div>
-            </div>
-          </div>
-
-          <div
-            v-if="allTodaysEvent(day, events).length > 3"
-            class="hidden md:flex mt-2 w-full px-2 py-1 space-x-2 items-center whitespace-nowrap overflow-hidden hover:text-gray-800 hover:font-medium cursor-pointer rounded-sm"
-            @click="openModal(day, allTodaysEvent(day, events))"
-          >
-            <div class="w-1/12">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-4 h-4"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 6v12m6-6H6"
-                />
-              </svg>
-            </div>
-            <div class="w-11/12">
-              <h6
-                class="text-xs tracking-tight text-clip text-left overflow-hidden"
-              >
-                {{ allTodaysEvent(day, events).length - 3 + " more events" }}
-              </h6>
-            </div>
-          </div>
-
-          <div
-            v-if="allTodaysEvent(day, events).length > 0"
-            class="flex md:hidden h-2/3 w-full justify-center items-center"
-            @click="openModal(day, allTodaysEvent(day, events))"
-          >
-            <div
-              class="h-6 w-6 flex justify-center items-center text-xs bg-purple-600 rounded-full shadow-sm"
-            >
-              <h3 class="font-medium text-white">
-                {{ allTodaysEvent(day, events).length }}
-              </h3>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div
-        v-if="lastEmptyCells > 0"
-        v-for="day in lastEmptyCells"
-        :key="day"
-        class="h-16 md:h-36 w-full border opacity-50"
-      ></div> -->
-
-  <!-- mobile navigation -->
-  <!-- <div class="md:hidden col-span-7 flex justify-between items-center p-2">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-5 h-5 hover:text-gray-500 cursor-pointer hover:h-6 hover:w-6 transition-all"
-            @click="calendarStore.decrementMonth(1)"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
-            />
-          </svg>
-        </div>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-5 h-5 hover:text-gray-500 cursor-pointer hover:h-6 hover:w-6 transition-all"
-            @click="calendarStore.incrementMonth(1)"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </div>
-      </div>
-    </div>
-  </div> -->
-
   <!-- use the modal component -->
   <transition name="modal">
     <Modal
@@ -307,7 +170,7 @@ import { useCalendarStore } from "../stores/calendar";
 import { usePopover } from "../composables/popover";
 import axios from "axios";
 
-let events = ref([]);
+const events = ref([]);
 
 /**
  * Gets event data
@@ -316,7 +179,7 @@ const getEventData = async () => {
   await axios
     .get(import.meta.env.VITE_API_DOMAIN + "/api/events?format=json")
     .then((response) => {
-      events = response.data;
+      events.value = response.data;
     });
 };
 
